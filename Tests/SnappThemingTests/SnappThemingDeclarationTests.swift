70386362f3bd7aae1c8976bc6ca712b4bc030de5
Voiko Volodymyr <vojkovladimir@gmail.com>
2025-01-07 14:19:43 +0200

    Grouped tests

---
+++ Tests/SnappThemingTests/SnappThemingDeclarationTests.swift
@@ -0,0 +1,59 @@
+import Testing
+@testable import SnappTheming
+import UIKit
+
+final class SnappThemingDeclarationTests {
+    @Test func parseDeclaration() async throws {
+        let json = """
+        {
+            "colors": {
+                "baseWhite": "#FFFFFF",
+                "baseBlack": "#000000"
+            }
+        }
+        """
+        let declaration = try SnappThemingParser.parse(from: json)
+        let baseWhite: UIColor = declaration.colors.baseWhite
+        let baseBlack: UIColor = declaration.colors.baseBlack
+        #expect(baseWhite == UIColor(hex: "FFFFFF", format: .rgba))
+        #expect(baseBlack == UIColor(hex: "000000", format: .rgba))
+    }
+    
+    @Test func manualDeclarationCreation() async throws {
+        let declaration = SnappThemingDeclaration(
+            colorCache: [
+                "baseWhite": .value(.hex("FFFFFF")),
+                "baseBlack": .value(.hex("000000"))])
+        let baseWhite: UIColor = declaration.colors.baseWhite
+        let baseBlack: UIColor = declaration.colors.baseBlack
+        #expect(baseWhite == UIColor(hex: "FFFFFF", format: .rgba))
+        #expect(baseBlack == UIColor(hex: "000000", format: .rgba))
+    }
+    
+    @Test func overrideDeclaration() async throws {
+        let baseJSON = """
+        {
+            "colors": {
+                "baseWhite": "#FFFFFF",
+                "baseBlack": "#000000"
+            }
+        }
+        """
+        let overrideJSON = """
+        {
+            "colors": {
+                "baseBlack": "#111111"
+            }
+        }
+        """
+        let baseDeclaration = try SnappThemingParser.parse(from: baseJSON)
+        let overrideDeclaration = try SnappThemingParser.parse(from: overrideJSON)
+        
+        let declaration = baseDeclaration.override(with: overrideDeclaration)
+        
+        let baseWhite: UIColor = declaration.colors.baseWhite
+        let baseBlack: UIColor = declaration.colors.baseBlack
+        #expect(baseWhite == UIColor(hex: "FFFFFF", format: .rgba))
+        #expect(baseBlack == UIColor(hex: "111111", format: .rgba))
+    }
+}
