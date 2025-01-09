00786cd69e46b90604ec5c3c74b4e60450d2fc15
Kolomiiets Oleksii <oleksii@snapp.team>
2025-01-09 12:02:30 +0100

    doc slider filers

--- Sources/SnappTheming/Theme/SliderStyle/SnappThemingSliderStyleConfiguration.swift
+++ Sources/SnappTheming/Theme/SliderStyle/SnappThemingSliderStyleConfiguration.swift
@@ -8,10 +8,18 @@
 import Foundation
 import SwiftUI
 
+/// A configuration structure that holds fallback values and related declarations for slider style elements such as track colors, font size, and tick mark color.
 public struct SnappThemingSliderStyleConfiguration {
+    /// The fallback color for the minimum track of the slider.
     public let fallbackMinimumTrackTintColor: Color
+
+    /// The fallback color for the maximum track of the slider.
     public let fallbackMaximumTrackTintColor: Color
+
+    /// The fallback font size for the slider's header and tick marks.
     public let fallbackFontSize: CGFloat
+
+    /// The fallback color for the tick marks on the slider.
     public let fallbackTickMarkColor: Color
 
     let typographies: SnappThemingTypographyDeclarations
