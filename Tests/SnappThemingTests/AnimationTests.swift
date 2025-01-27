62585c7b87adf03b4506425075ff1dda4f69d9e6
Voiko Volodymyr <vojkovladimir@gmail.com>
2025-01-27 21:46:01 +0200

    Test animations subscripts

--- Tests/SnappThemingTests/AnimationTests.swift
+++ Tests/SnappThemingTests/AnimationTests.swift
@@ -11,20 +11,19 @@ import Testing
 
 @Suite
 struct AnimationTests {
-    @Test
-    func testParsing() throws {
-        let json =
-            """
-            {
-                "animations": {
-                    "lego": {
-                        "type": "lottie",
-                        "value": "eyJ2IjoiNC44LjAiLCJtZXRhIjp7ImciOiJMb3R0aWVGaWxlcyBBRSAiLCJhIjoiIiwiayI6IiIsImQiOiIiLCJ0YyI6IiJ9LCJmciI6NjAsImlwIjowLCJvcCI6MTgwLCJ3IjoxMDI0LCJoIjoxMDI0LCJubSI6IkxFR08iLCJkZGQiOjAsImFzc2V0cyI6W10sImxheWVycyI6W3siZGRkIjowLCJpbmQiOjEsInR5Ijo0LCJubSI6IjEgIiwi"
-                    }
+    @Test(arguments: [
+        """
+        {
+            "animations": {
+                "lego": {
+                    "type": "lottie",
+                    "value": "eyJ2IjoiNC44LjAiLCJtZXRhIjp7ImciOiJMb3R0aWVGaWxlcyBBRSAiLCJhIjoiIiwiayI6IiIsImQiOiIiLCJ0YyI6IiJ9LCJmciI6NjAsImlwIjowLCJvcCI6MTgwLCJ3IjoxMDI0LCJoIjoxMDI0LCJubSI6IkxFR08iLCJkZGQiOjAsImFzc2V0cyI6W10sImxheWVycyI6W3siZGRkIjowLCJpbmQiOjEsInR5Ijo0LCJubSI6IjEgIiwi"
                 }
             }
-            """
-
+        }
+        """
+    ])
+    func testParsing(_ json: String) throws {
         let fallbackLottieAnimationData = "no-animation".data(using: .utf8)!
         let configuration = SnappThemingParserConfiguration(
             fallbackLottieAnimationData: fallbackLottieAnimationData
@@ -32,11 +31,13 @@ struct AnimationTests {
 
         let declaration = try SnappThemingParser.parse(from: json, using: configuration)
 
+        let animationRepresentation: SnappThemingAnimationRepresentation = try #require(declaration.animations.lego)
+        let animationValue: SnappThemingAnimationRepresentation.SnappThemingAnimationValue = declaration.animations.lego
         #expect(
-            declaration.animations.lego.data != fallbackLottieAnimationData,
+            animationValue.data != fallbackLottieAnimationData,
             "Parsed animation data should not match the fallback data.")
 
-        if case .lottie = declaration.animations.lego {
+        if case .lottie = animationRepresentation.animation {
             // Test passes if the animation is of type `.lottie`
         } else {
             Issue.record("Animation should be lottie")
