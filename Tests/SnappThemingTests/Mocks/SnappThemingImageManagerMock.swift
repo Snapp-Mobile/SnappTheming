36009a1b0870c12a07ddffb4081fc98c7cffb612
Voiko Volodymyr <vojkovladimir@gmail.com>
2025-01-28 15:24:01 +0200

    Improve images code coverage

--- Tests/SnappThemingTests/Mocks/SnappThemingImageManagerMock.swift
+++ Tests/SnappThemingTests/Mocks/SnappThemingImageManagerMock.swift
@@ -10,8 +10,17 @@ import UIKit
 import UniformTypeIdentifiers
 
 final class SnappThemingImageManagerMock: SnappThemingImageManager {
-    let cache: NSCache<NSString, UIImage> = .init()
+    let cache: NSCache<NSString, UIImage>
     private let accessQueue = DispatchQueue(label: "SharedCacheAccess")
+    let image: @Sendable (Data, UTType) -> UIImage?
+
+    init(
+        cache: NSCache<NSString, UIImage> = .init(),
+        image: @escaping @Sendable (Data, UTType) -> UIImage? = { data, _ in UIImage(data: data) }
+    ) {
+        self.cache = cache
+        self.image = image
+    }
 
     func object(for key: String, of dataURI: SnappThemingDataURI) -> UIImage? {
         accessQueue.sync {
@@ -30,6 +39,6 @@ final class SnappThemingImageManagerMock: SnappThemingImageManager {
     }
 
     func image(from data: Data, of type: UTType) -> UIImage? {
-        UIImage(data: data)
+        image(data, type)
     }
 }
