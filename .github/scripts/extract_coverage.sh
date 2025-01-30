f29299719adf7436b728b218c131dd46295aa186
Oleksii Kolomiiets <oleksii@snapp.team>
2025-01-30 14:29:01 +0100

    fix typo and update table

--- .github/scripts/extract_coverage.sh
+++ .github/scripts/extract_coverage.sh
@@ -10,19 +10,14 @@ FILES_LINE_COUNTS=$(jq -r '.data[0].files[] | select(.filename | contains("Snapp
 
 # Initialize variables for total lines, covered lines, and count
 total_lines=0
-covered_lines=0
-count=0
 
 # Loop through each file's line count data (FILES_LINE_COUNTS)
 for lines_data in $(echo "$FILES_LINE_COUNTS" | jq -c '.'); do
   # Extract total and covered lines for each file
   total=$(echo "$lines_data" | jq '.count')
-  covered=$(echo "$lines_data" | jq '.covered')
 
   # Add to the total lines and covered lines
   total_lines=$((total_lines + total))
-  covered_lines=$((covered_lines + covered))
-  count=$((count + 1))  # Increment the count of files
 done
 
 # Calculate the average line coverage percentage
@@ -35,11 +30,11 @@ fi
 # Generate coverage badge URL
 average_coverage_rounded=$(echo "$average_coverage" | awk '{print int($1 * 100 + 0.5) / 100}')
 # Generate coverage badge URL
-average_coverage_with_percentage="${average_coverage_rounded}&"
+average_coverage_with_percentage="${average_coverage_rounded}%"
 
 # Save to pr_coverage_summary.txt
 cat <<EOF > pr_coverage_summary.txt
-| ID | Name | Source Files | Lines | Coverage |
-|----|------|--------------|-------|----------|
-| 0 | SnappTheming | $count | ($covered_lines/$total_lines) | $average_coverage_with_percentage |
+| ID | Name | Executable Lines | Coverage |
+|----|------|------------------|----------|
+| 0 | SnappTheming | $total_lines | $average_coverage_with_percentage |
 EOF
