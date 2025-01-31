02d2ffd94b2e237b32109bdc56907f27c1f0edcb
Voiko Volodymyr <vojkovladimir@gmail.com>
2025-01-31 16:41:04 +0200

    Partly apply button styling

--- Example/Example/Example/Modules/Banking/ButtonStyles/ActionButtonStyle.swift
+++ Example/Example/Example/Modules/Banking/ButtonStyles/ActionButtonStyle.swift
@@ -6,41 +6,59 @@
 //
 
 import Foundation
+import SnappTheming
 import SwiftUI
 
+// TODO: Create button style in theme
+
 struct ActionButtonStyle: ButtonStyle {
+    private let declaration: SnappThemingDeclaration
+
+    init(declaration: SnappThemingDeclaration) {
+        self.declaration = declaration
+    }
+
     func makeBody(configuration: Configuration) -> some View {
-        configuration.label.labelStyle(.actionButton)
+        configuration.label.labelStyle(.actionButton(declaration))
     }
 }
 
 struct ActionButtonLabelStyle: LabelStyle {
-    private let iconSize: CGFloat = 24
-    private let buttonSize: CGFloat = 64
+    private let declaration: SnappThemingDeclaration
+
+    init(declaration: SnappThemingDeclaration) {
+        self.declaration = declaration
+    }
 
     func makeBody(configuration: Configuration) -> some View {
-        VStack(spacing: 8) {
+        let iconSize = declaration.metrics.iconSize
+        let buttonSize = declaration.metrics.buttonSize
+        VStack(spacing: declaration.metrics.small) {
             ZStack {
                 configuration.icon
                     .frame(width: iconSize, height: iconSize)
             }
             .frame(width: buttonSize, height: buttonSize)
-            .background(Circle().fill(Color.white))
+            .background(Circle().fill(declaration.colors.surfacePrimary))
             .shadow(color: .gray.opacity(0.1), radius: 12, x: 0, y: 4)
 
             configuration.title
-                .font(.caption)
-                .foregroundColor(.black)
+                .font(declaration.typography.body)
+                .foregroundStyle(declaration.colors.textColorPrimary)
         }
     }
 }
 
 extension LabelStyle where Self == ActionButtonLabelStyle {
-    static var actionButton: Self { .init() }
+    static func actionButton(_ declaration: SnappThemingDeclaration) -> Self {
+        .init(declaration: declaration)
+    }
 }
 
 extension ButtonStyle where Self == ActionButtonStyle {
-    static var actionButton: Self { .init() }
+    static func actionButton(_ declaration: SnappThemingDeclaration) -> Self {
+        .init(declaration: declaration)
+    }
 }
 
 #Preview {
@@ -52,6 +70,6 @@ extension ButtonStyle where Self == ActionButtonStyle {
                 Image(systemName: "xmark")
             }
         }
-        .buttonStyle(.actionButton)
+        .buttonStyle(.actionButton(.bankingLight))
     }
 }
