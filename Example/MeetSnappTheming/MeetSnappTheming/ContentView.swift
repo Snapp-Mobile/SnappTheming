8e6d24c1a116fe25f8c3ecf74361952045a9d812
Voiko Volodymyr <vojkovladimir@gmail.com>
2025-02-11 14:56:27 +0200

    Add sample project for interactive documentation

---
+++ Example/MeetSnappTheming/MeetSnappTheming/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  MeetSnappTheming
+//
+//  Created by Volodymyr Voiko on 11.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        EmptyView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
