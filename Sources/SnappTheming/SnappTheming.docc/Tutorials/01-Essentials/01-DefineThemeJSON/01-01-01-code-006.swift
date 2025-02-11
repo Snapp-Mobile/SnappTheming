99be089cffe06c305b06ce8b9399be9639e1e0b7
Voiko Volodymyr <vojkovladimir@gmail.com>
2025-02-11 17:47:46 +0200

    Add step 01-01-06

---
+++ Sources/SnappTheming/SnappTheming.docc/Tutorials/01-Essentials/01-DefineThemeJSON/01-01-01-code-006.swift
@@ -0,0 +1,53 @@
+import SnappTheming
+
+private let json = """
+    {
+        "colors": {
+            "primary": "#FF0000",
+            "textPrimary": "#000",
+            "textSecondary": "#CCC",
+            "surfacePrimary": "#FFF",
+            "surfaceSecondary": "#EEE"
+        },
+        "fonts": {
+            "Doto-Regular": {
+                "postScriptName": "Doto-Regular",
+                "source": "data:font/ttf;base64,AAEAAA*******EBMgEA"
+            },
+            "Doto-SemiBold": {
+                "postScriptName": "Doto-SemiBold",
+                "source": "data:font/ttf;base64,AAEAAA*******IBAAAA"
+            },
+            "Doto-Bold": {
+                "postScriptName": "Doto-Bold",
+                "source": "data:font/ttf;base64,AAEAAA*******kBAAAA"
+            }
+        },
+        "typography": {
+            "title": {
+                "font": "$fonts/Doto-Bold",
+                "fontSize": 16
+            },
+            "largeTitle": {
+                "font": "$fonts/Doto-Bold",
+                "fontSize": 18
+            },
+            "headline": {
+                "font": "$fonts/Doto-Bold",
+                "fontSize": 14
+            },
+            "subheadline": {
+                "font": "$fonts/Doto-SemiBold",
+                "fontSize": 12
+            },
+            "body": {
+                "font": "$fonts/Doto-Regular",
+                "fontSize": 14
+            }
+        }
+    }
+    """
+
+extension SnappThemingDeclaration {
+    static let light = try! SnappThemingParser.parse(from: json)
+}
