c7fa8dd49f84f6c646a7dff6baea3b5fef26f3f7
Oleksii Kolomiiets <oleksii@snapp.team>
2025-02-17 18:30:02 +0100

    decrease amount of lines to see how this results to coverage

--- Sources/SnappTheming/Theme/Util/RuntimeWarning.swift
+++ Sources/SnappTheming/Theme/Util/RuntimeWarning.swift
@@ -20,24 +20,13 @@ internal func runtimeWarning(_ message: String) {
             var info = Dl_info()
             dladdr(
                 dlsym(
-                    dlopen(nil, RTLD_LAZY),
-                    """
-                    $s10Foundation15AttributeScopesO7SwiftUIE05swiftE0AcDE0D12UIAttributesVmvg
-                    """
-                ),
-                &info
-            )
+                    dlopen(nil, RTLD_LAZY), "$s10Foundation15AttributeScopesO7SwiftUIE05swiftE0AcDE0D12UIAttributesVmvg"
+                ), &info)
             // Find the warning in the Issue Navigator (⌘5) to view the full stack trace captured at the moment the warning was logged.
             // Selecting element number '1' in the stack trace will take you to the exact location where the warning was triggered.
             os_log(
-                .fault,
-                dso: info.dli_fbase,
-                log: OSLog(
-                    subsystem: "com.apple.runtime-issues",
-                    category: "SnappTheming"
-                ),
-                "%@", message
-            )
+                .fault, dso: info.dli_fbase,
+                log: OSLog(subsystem: "com.apple.runtime-issues", category: "SnappTheming"), "%@", message)
         } else {
             os_log(.debug, "%@", message)
         }
