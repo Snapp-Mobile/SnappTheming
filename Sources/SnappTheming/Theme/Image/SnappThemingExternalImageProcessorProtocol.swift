3f2770c8add5cca401e11cfb5dec2e14aee48f45
Oleksii Kolomiiets <oleksii@snapp.team>
2025-02-24 17:22:48 +0100

    improve SnappThemingExternalImageProcessorProtocol to use SnappThemingImage

--- Sources/SnappTheming/Theme/Image/SnappThemingExternalImageProcessorProtocol.swift
+++ Sources/SnappTheming/Theme/Image/SnappThemingExternalImageProcessorProtocol.swift
@@ -8,30 +8,12 @@
 import SwiftUI
 import UniformTypeIdentifiers
 
-#if canImport(UIKit)
-    import UIKit
-#elseif canImport(AppKit)
-    import AppKit
-#endif
-
-#if canImport(UIKit)
-    /// A protocol defining a processor for custom image types, enabling conversion of `SnappThemingDataURI` to `UIImage`.
-    public protocol SnappThemingExternalImageProcessorProtocol: Sendable {
-        /// Processing image `Data` and `UIType` to convert into a `UIImage`.
-        ///
-        /// - Parameter data: Image `Data`.
-        /// - Parameter type: Image `UTType`.
-        /// - Returns: A `UIImage` if the conversion is successful; otherwise, `nil`.
-        func process(_ data: Data, of type: UTType) -> UIImage?
-    }
-#elseif canImport(AppKit)
-    /// A protocol defining a processor for custom image types, enabling conversion of `SnappThemingDataURI` to `NSImage`.
-    public protocol SnappThemingExternalImageProcessorProtocol: Sendable {
-        /// Processing image `Data` and `UIType` to convert into a `NSImage`.
-        ///
-        /// - Parameter data: Image `Data`.
-        /// - Parameter type: Image `UTType`.
-        /// - Returns: A `NSImage` if the conversion is successful; otherwise, `nil`.
-        func process(_ data: Data, of type: UTType) -> NSImage?
-    }
-#endif
+/// A protocol defining a processor for custom image types, enabling conversion of ``SnappThemingDataURI`` to ``SnappThemingImage`` .
+public protocol SnappThemingExternalImageProcessorProtocol: Sendable {
+    /// Processing image `Data` and `UIType` to convert into a ``SnappThemingImage``.
+    ///
+    /// - Parameter data: Image `Data`.
+    /// - Parameter type: Image `UTType`.
+    /// - Returns: A `SnappThemingImage` if the conversion is successful; otherwise, `nil`.
+    func process(_ data: Data, of type: UTType) -> SnappThemingImage?
+}
