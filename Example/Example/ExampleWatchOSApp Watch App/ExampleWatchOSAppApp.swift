359bde68016d433a07b5253a9011f9cfccb35aff
Oleksii Kolomiiets <oleksii@snapp.team>
2025-02-25 13:22:00 +0100

    applied latest changes of theme management to watch app

--- Example/Example/ExampleWatchOSApp Watch App/ExampleWatchOSAppApp.swift
+++ Example/Example/ExampleWatchOSApp Watch App/ExampleWatchOSAppApp.swift
@@ -10,10 +10,24 @@ import SwiftUI
 
 @main
 struct ExampleWatchOSApp_Watch_AppApp: App {
+    @State var theme: Theme
+
+    private let settingsManager: SettingsManager
+    private let storage: SettingsManager.Storage
+
+    init() {
+        storage = .userDefaults()
+
+        settingsManager = SettingsManager(storage: .userDefaults(), fallbackColorSchema: .dark)
+        theme = Theme(settingsManager.themeSource)
+    }
+
     var body: some Scene {
         WindowGroup {
             MainView()
-                .themed()
+                .themed(with: settingsManager, theme: $theme)
+                .environment(settingsManager)
+                .environment(theme)
         }
     }
 }
